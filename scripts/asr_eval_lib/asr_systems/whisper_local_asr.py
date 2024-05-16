3947108ba9a38353a227c92581d48dff9f15a0ac
mj-new <>
2024-05-16 22:55:51 +0200

    Updated local whisper config to CPU

--- scripts/asr_eval_lib/asr_systems/whisper_local_asr.py
+++ scripts/asr_eval_lib/asr_systems/whisper_local_asr.py
@@ -5,7 +5,7 @@ class WhisperLocalASR(BaseASRSystem):
     def __init__(self, system, model, language_code:str = "pl-PL",sampling_rate:int = 16000) -> None:
         super().__init__(system, model, language_code)
         self.whisper_local_language = self.language_code.split("-")[0]
-        self.whisper_local_model = whisper.load_model(model)  # You can choose different model sizes
+        self.whisper_local_model = whisper.load_model(model, device="cpu")  # You can choose different model sizes
 
     def generate_asr_hyp(self, speech_file):
         try:
@@ -14,6 +14,7 @@ class WhisperLocalASR(BaseASRSystem):
             print("Hyp:", hyp)
         except Exception as e:
             print(f"Other error: {e}")
+            exit()
         
         self.update_cache(speech_file, hyp)
         return hyp
