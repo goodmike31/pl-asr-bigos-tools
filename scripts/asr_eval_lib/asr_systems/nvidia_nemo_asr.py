9736e593c8d18d80ae2fe157f8dd2f00871336bc
mj-new <>
2024-10-13 17:41:33 +0200

    Added license and refactored nvidia ASR system class

--- scripts/asr_eval_lib/asr_systems/nvidia_nemo_asr.py
+++ scripts/asr_eval_lib/asr_systems/nvidia_nemo_asr.py
@@ -1,12 +1,17 @@
 from .base_asr_system import BaseASRSystem
 import nemo.collections.asr as nemo_asr
 
-
 class NvidiaNemoASR(BaseASRSystem):
     def __init__(self, system, model, language_code:str = "pl-PL",sampling_rate:int = 16000) -> None:
         super().__init__(system, model, language_code)
-        self.nemo_asr_model = nemo_asr.models.EncDecHybridRNNTCTCBPEModel.from_pretrained(model_name="nvidia/stt_pl_fastconformer_hybrid_large_pc")
-
+        # check if model name contains "fastconformer" in its name to determine model loading method
+        if "fastconformer" in model:
+            self.nemo_asr_model = nemo_asr.models.EncDecHybridRNNTCTCBPEModel.from_pretrained(model_name=model)
+        elif "quartznet" in model:
+            self.nemo_asr_model = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name=model)
+        else:
+            raise ValueError(f"Unknown model type: {model}")
+        
     def generate_asr_hyp(self, speech_file):
         try:
             hyp = self.nemo_asr_model.transcribe(paths2audio_files=[speech_file])[0][0]
